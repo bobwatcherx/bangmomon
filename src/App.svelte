ce8a897ef317ec738e0a796edbfc6a69de61b636
root <bobwatcherx@gmail.com>
2023-10-20 15:16:25 +0700

    form

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,22 @@
   import { Router, Link, Route } from "svelte-navigator";
   import Home from './lib/Home.svelte'
   import Player from './lib/Player.svelte'
+  import Request from './lib/Request.svelte'
   import Navbarmenu from './lib/util/Navbarmenu.svelte'
-
 </script>
 <Router>
   <div>
     <Navbarmenu/>
+    <div class="m-2">
+      <Link 
+      class="btn btn-danger"
+      to="/request">Request Bokep Jepang Disini</Link>
+    </div>
     <Route path="/">
       <Home />
+    </Route>
+     <Route path="/request">
+      <Request />
     </Route>
     <Route path="/player/:id" let:params>
       <Player id={params.id} />
---
+++ src/lib/Request.svelte
@@ -0,0 +1,10 @@
+<div class="mt-3 m-2">
+	<p>Kami Menyediakan bokep bokep jepang
+		terbaru . termontok , terlegit . yang mau request
+		artis artis bokep bisa isi surat bokep di bawah ini . kami akan selalu upload dan membaca surat bokep dari kalian. ayokkk
+	</p>
+</div>
+
+<div class="m-2">
+	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfxRYsKXvXMSBwqIMQJ-PWDhDjPJEXjpL8IixBLv9fpSLQVGg/viewform?embedded=true" width="100%" height="500px" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+</div>
