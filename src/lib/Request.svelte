a17fb89519b8056c0ac31df45317662175950a26
root <bobwatcherx@gmail.com>
2023-10-22 12:08:12 +0700

    3213

--- src/lib/Request.svelte
+++ src/lib/Request.svelte
@@ -7,11 +7,11 @@
 </div>
 <div class="mt-3 m-2">
 	<p>Kami Menyediakan bokep bokep jepang
-		terbaru . termontok , terlegit . yang mau request
-		artis artis bokep bisa isi surat bokep di bawah ini . kami akan selalu upload dan membaca surat bokep dari kalian. ayokkk
+		 termontok , tersemok , terlegit . yang mau request
+		artis artis bokep bisa isi surat bokep di bawah ini . kami akan selalu upload dan membaca surat bokep dari kalian. gasss lah
 	</p>
 </div>
 
 <div class="m-2">
-	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfxRYsKXvXMSBwqIMQJ-PWDhDjPJEXjpL8IixBLv9fpSLQVGg/viewform?embedded=true" width="100%" height="500px" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfQuRWcSlgRy-sx6tpSEeBAnZHRh_ttkjMi0sRZhECx1OhsoA/viewform?embedded=true" width="100%" height="375" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
 </div>
