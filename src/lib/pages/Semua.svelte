2ede85e68ebe49e2e68240f28abc67eac645aad4
root <bobwatcherx@gmail.com>
2023-10-22 12:06:18 +0700

    3213

--- src/lib/pages/Semua.svelte
+++ src/lib/pages/Semua.svelte
@@ -2,17 +2,18 @@
   import { onMount } from 'svelte';
   import { Alert, Button, Card, CardText, CardTitle, Pagination } from 'sveltestrap';
   import { Link } from 'svelte-navigator'
-import {Spinner} from 'sveltestrap'
+  import { Spinner } from 'sveltestrap'
+
   let data = null;
   let currentPage = 1;
-  let perPage = 35;
-  let isLoading = true; // New variable to track loading state
+  let perPage = 20;
+  let isLoading = true;
 
   async function fetchData(page, perPage) {
-    const response = await fetch(`https://api.filelions.com/api/file/list?key=1730m2zzxzv2tzii249x&page=${page}&per_page=${perPage}`);
+    const response = await fetch(`https://filemoon.sx/api/file/list?key=26604oo759r7ar6j96q7h&page=${page}&per_page=${perPage}`);
     const json = await response.json();
-    data = json.result.files;
-    isLoading = false; // Set loading state to false when data is loaded
+    data = json.result;
+    isLoading = false;
   }
 
   onMount(() => {
@@ -35,42 +36,34 @@ import {Spinner} from 'sveltestrap'
 <main>
   <div class="container">
     {#if isLoading}
-    <div style="display: flex;justify-content: center;"
-    class="m-3" 
-    >
-      <Spinner
-    style="color:orange;width: 100px;height: 100px;"
-    />
-    </div>
-    <p style="font-weight: bold;text-align: center;">Sedang Menyiapkan data ...</p>
-
-    {:else}
-    <div class="row">
-      {#each data as item (item.thumbnail)}
-       <Link to={`/player/${item.file_code}`}>
-       <div class="col-md-3 col-12">
-          <Card class="p-1 mt-3" style="background-color: #d9d2d6;">
-          <img src={item.thumbnail} class="card-img-top" alt="File Thumbnail">
-          <CardTitle>{item.title}</CardTitle>
-          <CardText>
-            <p>Ditonton : {item.views}</p>
-            <p>Diupload : {item.uploaded}</p>
-          </CardText>
-          <Link to={`/player/${item.file_code}`}  class="btn "
-        style="background-color: orange;
-        color: black;
-        font-weight: bold;"
-          >Nonton</Link>
-        </Card>
-       </div>
-     </Link>
-      {/each}
-       <div class="d-flex justify-content-between mt-3">
-        <Button on:click={previousPage} disabled={currentPage === 1}>Previous</Button>
-        <Button on:click={nextPage}>Next</Button>
+      <div style="display: flex; justify-content: center;" class="m-3">
+        <Spinner style="color: #e8139d; width: 100px; height: 100px;" />
       </div>
+      <p style="font-weight: bold; text-align: center;">Sedang Menyiapkan data ...</p>
+    {:else}
+      <div class="row">
+        {#each data.files as item (item.file_code)}
+          <div class="col-md-3 col-12">
+          <Link to={`/player/${item.file_code}`} >
+            <Card class="p-1 mt-3" style="background-color: #d9d2d6;">
+              <img src={item.thumbnail} class="card-img-top" alt="File Thumbnail">
+              <CardTitle>{item.title}</CardTitle>
+              <CardText>
+                <p>Ditonton : {item.views}</p>
+                <p>Diupload : {item.uploaded}</p>
+              </CardText>
+              <Link to={`/player/${item.file_code}`} class="btn" style="background-color: #e8139d; color: white; font-weight: bold;
+
+              ">Nonton sekarang</Link>
+            </Card>
+        </Link>
+          </div>
+        {/each}
+        <div class="d-flex justify-content-between mt-3">
+          <Button on:click={previousPage} disabled={currentPage === 1}>Previous</Button>
+          <Button on:click={nextPage}>Next</Button>
+        </div>
       </div>
     {/if}
   </div>
 </main>
-
--- src/lib/util/Navbarmenu.svelte
+++ src/lib/util/Navbarmenu.svelte
@@ -1,3 +1,3 @@
-<div class="p-2" style="background-color:#b700ff">
-	<h4 style="font-weight:bold">Bang UCUP</h4>
+<div class="p-2" style="background-color:#bd2688">
+	<h4 style="font-weight:bold;color: white;">Bang Momon</h4>
 </div>
